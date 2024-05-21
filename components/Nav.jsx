621a6df9b873668a0ee167e1fa7a0f5138d8399f
Hanif <hanif@Hanifs-MacBook-Air.local>
2024-05-21 18:02:58 +0600

    header section done

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+"use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+
+  {
+    name: "resume",
+    path: "/resume",
+  },
+
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
 function Nav() {
+  const pathname = usePathname();
+
   return (
-    <nav>
-      desktop Nav
-      <h1></h1>
-      <h2></h2>
+    <nav className="flex gap-8">
+      {links.map((link, index) => {
+        return (
+          <Link
+            key={index}
+            href={link.path}
+            className={`${
+              link.path === pathname &&
+              "to-accent-hover border-b-2 border-accent-hover"
+            } capitalize font-medium hover:text-accent-hover transition-all`}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
     </nav>
   );
 }
