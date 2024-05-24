ab6415c78f246bcd056e3a370a304465b5d5e9ea
Hanif <hanif@Hanifs-Air.Dlink>
2024-05-24 12:18:17 +0600

    fixing some styles issue

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -22,7 +22,7 @@ const stats = [
 ];
 const Stats = () => {
   return (
-    <section className="pt-4 pb-12 lg:pt-0 lg:pb-0">
+    <section className="pt-4 pb-12 lg:pt-0">
       <div className="container mx-auto">
         <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto lg:max-w-none">
           {stats.map((item, index) => {
