dd7e56cd5adcf04795c5ba047aaed75238b0c665
Hanif <hanif@Hanifs-Air.Dlink>
2024-05-24 09:35:31 +0600

    working on contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { FaPhoneAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+880) 1607 150 462",
+  },
+];
+
 const Contact = () => {
   return <div>Contact</div>;
 };
