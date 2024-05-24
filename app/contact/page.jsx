f3993f9e0f9876a29d9f6b4c8df3ed0925fcb691
Hanif <hanif@Hanifs-Air.Dlink>
2024-05-24 12:13:48 +0600

    contact page done

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,19 @@
 "use client";
 
-import { FaPhoneAlt } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { motion } from "framer-motion";
+import { FaEnvelope, FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
 
 const info = [
   {
@@ -8,10 +21,97 @@ const info = [
     title: "Phone",
     description: "(+880) 1607 150 462",
   },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "iamhanif101@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: "Dhaka, Bangladesh",
+  },
 ];
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-8 lg:flex-row">
+          {/* form */}
+          <div className="order-2 lg:w-[54%] lg:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className=" text-4xl text-accent-hover">
+                Let&apos;s work together
+              </h3>
+              <p className=" text-white/60">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Aliquid impedit id vel nulla magnam?
+              </p>
+
+              {/* input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="Firstname" />
+                <Input type="lastname" placeholder="Lastname" />
+                <Input type="email" placeholder="Email address" />
+                <Input type="phone" placeholder="Phone number" />
+              </div>
+
+              {/* select */}
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="est">Web Developement</SelectItem>
+                    <SelectItem value="cst">Digital Marketing</SelectItem>
+                    <SelectItem value="mst">SEO</SelectItem>
+                    <SelectItem value="gst">Logo Design</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              {/* text area */}
+              <Textarea
+                className=" h-52"
+                placeholder="Type your message here."
+              />
+              {/* button */}
+              <Button size="md" className="max-w-40">
+                Send message
+              </Button>
+            </form>
+          </div>
+          {/* info */}
+          <div className=" flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className=" w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent-hover rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <h3 className="text-white/60">{item.title}</h3>
+                      <p className="text-xl">{item.description}</p>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contact;
